961d56ed1d4689805d5bca8c201e544ecdf3659b
stephanto-dev <v.stephanto@gmail.com>
2025-12-10 08:52:10 -0300

    adiciona `lint:check` e `lint:fix`

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,10 @@
 function Home() {
-    return <h2>Dessa, você é que nem o Github. Já viu várias versões minhas, mas sempre terá a melhor delas. Amo você!</h2>;
+  return (
+    <h2>
+      Dessa, você é que nem o Github. Já viu várias versões minhas, mas sempre
+      terá a melhor delas. Amo você!
+    </h2>
+  );
 }
 
 export default Home;
