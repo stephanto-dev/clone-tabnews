715397427e4944dc2e6aebbc84f628ebebf997ce
stephanto-dev <v.stephanto@gmail.com>
2026-02-26 08:28:58 -0300

    add database to file

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,5 +1,6 @@
 import migrationRunner from "node-pg-migrate";
 import { join } from "node:path";
+import database from "infra/database";
 
 export default async function migrations(request, response) {
   const dbClient = await database.getNewClient();
